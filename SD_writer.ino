12eee5e93b1261648bc7589b43786f901876c4db
Argonautex <74357760+Argonautex@users.noreply.github.com>
2021-02-06 11:57:21 +0000

    Update SD_writer.ino

--- SD_writer.ino
+++ SD_writer.ino
@@ -6,17 +6,12 @@ void setup()
 {
   Serial.begin(9600);
   Serial.print(F("Iniciando SD ..."));
-  if (!SD.begin(4))
+  if (!SD.begin(9))
   {
     Serial.println(F("Error al iniciar"));
     return;
   }
   Serial.println(F("Iniciado correctamente"));
-}
-
-int readSensor()
-{
-   return 0;
 }
  
 void loop()
@@ -24,11 +19,12 @@ void loop()
   logFile = SD.open("datalog.txt", FILE_WRITE);
   
   if (logFile) { 
-        int value = readSensor;
+        int value = 0;
         logFile.print("Time(ms)=");
         logFile.print(millis());
         logFile.print(", value=");
         logFile.println(value);
+        Serial.print(logFile.read());
         
         logFile.close();
   
