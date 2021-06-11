fbf3b2e3f73d24b8a597f17a53d227bac8215aae
Argonautex <74357760+Argonautex@users.noreply.github.com>
2021-06-11 08:10:27 +0100

    Add files via upload

---
+++ mkrwifi1000.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+
+void setup() {
+  Wire.setClock(100000);
+  Wire.begin(4);
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(100);
+}
+
+void receiveEvent(int howMany)
+{
+  while(Wire.available())
+  {
+    char c = Wire.read();
+    Serial.print(c); 
+  } 
+    Serial.println("");
+}
