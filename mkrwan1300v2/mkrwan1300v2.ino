9dc3e37781f0e6afb40bc54ee058dcf79dc1c85c
Argonautex <74357760+Argonautex@users.noreply.github.com>
2021-06-16 18:01:15 +0100

    Update mkrwan1300v2.ino

--- mkrwan1300v2/mkrwan1300v2.ino
+++ mkrwan1300v2/mkrwan1300v2.ino
@@ -3,14 +3,26 @@
 #include <SD.h>
 #include <Wire.h>
 #include <AES.h>
+#include <Adafruit_GPS.h>
+#include <math.h>
 
 File Fichero;
 AES aes;
 
+#define GPSSerial Serial1
+
+Adafruit_GPS GPS(&GPSSerial);
+
+#define GPSECHO false
+
 byte *key = (unsigned char*)"0123456789010123";
 unsigned long long int my_iv = 36753562;
-
+String datos[22];
+String datosGPS[2];
+String distancia;
 String cadena_envio;
+String prov;
+int contador;
 
 void setup() {
   Serial.begin(115200);
@@ -25,6 +37,13 @@ void setup() {
   }
   while (!Serial);
 
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  GPS.sendCommand(PGCMD_ANTENNA);
+  delay(1000);
+  GPSSerial.println(PMTK_Q_RELEASE);
+
   if (!LoRa.begin(868E6)) {
     Serial.println("Error al iniciar LoRa");
     while (1);
@@ -38,6 +57,17 @@ void setup() {
 }
 
 void loop() {
+  contador = 0;
+  prov = "";
+  char c = GPS.read();
+  if (GPSECHO)
+    if (c) Serial.print(c);
+  if (GPS.newNMEAreceived()) {
+    Serial.print(GPS.lastNMEA());
+    if (!GPS.parse(GPS.lastNMEA()))
+      return;
+  }
+
   int packetSize = LoRa.parsePacket();
   String cadena;
   if (packetSize) {
@@ -58,19 +88,80 @@ void loop() {
     cadena = (char *)check;
     Serial.println(cadena);
 
-    Fichero = SD.open("cansat.csv", FILE_WRITE);
-    Fichero.println(cadena);
-    Fichero.close();
-
-    cadena_envio = "";
-    for (int h=0;h<31;h++){
-      cadena_envio.concat(cadena[h]);
+    for (int h = 0; h < cadena.length(); h++) {
+      if (cadena[h] != ',') {
+        prov = prov + cadena[h];
+      } else if (cadena[h] == ',') {
+        datos[contador] = prov;
+        prov = "";
+        contador ++;
+      }
     }
 
-    Wire.beginTransmission(4);
-    Wire.write(cadena_envio.c_str());
-    Wire.endTransmission();
-    Wire.flush();
-    Serial.println("Enviado por wire");
+    if (datos[0] == "ArgonSat") {
+
+      Fichero = SD.open("cansat.csv", FILE_WRITE);
+      Fichero.println(cadena);
+      Fichero.close();
+
+
+
+      calcularDistancia(datos[6].toFloat(), datos[7].toFloat());
+
+      cadena_envio = "";
+      for (int h = 0; h < 31; h++) {
+        cadena_envio.concat(cadena[h]);
+      }
+
+      Wire.beginTransmission(4);
+      Wire.write(cadena_envio.c_str());
+      Wire.endTransmission();
+      Wire.flush();
+      Serial.println("Enviado por wire");
+    }
+  }
+}
+
+void calcularDistancia(float suLatitud, float suLongitud) {
+  if (GPS.fix) {
+    float distlat = ((GPS.latitude / 60) - (suLatitud / 60));
+    float distlon = ((GPS.longitude / 60) - (suLongitud / 60));
+
+    if (distlat < 0) {
+      distlat *= -1;
+    }
+    if (distlon < 0) {
+      distlon *= -1;
+    }
+
+    Serial.print(distlat);
+    Serial.println("º");
+    Serial.print(distlon);
+    Serial.println("º");
+
+    distlat *= 96.225;
+    distlon *= 111.3194;
+
+    float distancia = sqrt(pow(distlat, 2) + pow(distlon, 2));
+    distancia *= 1000;
+    distancia -= 12;
+    Serial.print(distancia);1
+    
+    Serial.println("m");
+    distancia /= 1000;
+
+    Serial.print(distlat);
+    Serial.println("km");
+    Serial.print(distlon);
+    Serial.println("km");
+
+    float dif = distlat / distancia;
+
+    float alpha = acos(dif);
+    float angulo = (alpha * 180) / PI;
+    Serial.print(angulo);
+    Serial.println("º con respecto al norte");
+
+    Serial.print("\n");
   }
 }
