659830c012f86f0032c7f2f8a3d3c0b6d78e991d
Argonautex <74357760+Argonautex@users.noreply.github.com>
2021-06-19 10:33:03 +0100

    Update TTGO_vuelo.ino

--- TTGO_vuelo.ino
+++ TTGO_vuelo.ino
@@ -14,7 +14,7 @@
 
 // Pines para la OLED
 #define OLED_SDA 4
-#define OLED_SCL 15 
+#define OLED_SCL 15
 #define OLED_RST 16
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -23,7 +23,7 @@
 #define BANDA 868E6
 
 // Declaramos las variables
-int angulo1 = 0;  
+int angulo1 = 0;
 int angulo2 = 0;
 
 // Contador de paquetes LoRa
@@ -32,12 +32,12 @@ int counter = 0;
 // Creamos un objeto de pantalla para la OLED
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
 
-void setup() 
+void setup()
 {
   // Iniciamos el Serial a 115200 baudios y el LoRa
   Serial.begin(115200);
   iniciar_Lora();
-  
+
   // Reseteamos la OLED
   pinMode(OLED_RST, OUTPUT);
   digitalWrite(OLED_RST, LOW);
@@ -46,22 +46,22 @@ void setup()
 
   // Iniciamos la OLED
   Wire.begin(OLED_SDA, OLED_SCL);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) 
-  { 
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false))
+  {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
+    for (;;);
   }
   display.clearDisplay();
   display.setTextColor(WHITE);
   display.setTextSize(1);
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.print("CONTROL VUELO");
   display.display();
 }
-   
-void loop() 
+
+void loop()
 {
-   // Leemos el Joystick
+  // Leemos el Joystick
   int joy = analogRead(A1);
 
   if (joy <= 2770)
@@ -73,26 +73,26 @@ void loop()
   if (joy >= 2770)
   {
     angulo2 = map(joy, 2749, 4095, 0, 45);
-  }else{
+  } else {
     angulo2 = 0;
   }
-  
+
   String ang1 = String(angulo1);
   String ang2 = String(angulo2);
-  
+
   Serial.println(joy);
   Serial.println(ang1);
   Serial.println(ang2);
 
-  
+
   String cadena;
   cadena.concat("cva");
-  cadena.concat(","); 
+  cadena.concat(",");
   cadena.concat(ang1);
   cadena.concat(",");
   cadena.concat(ang2);
   cadena.concat(",");
- 
+
   // Envio de paquetes
   Serial.print("Mandando paquete: ");
   Serial.println(counter);
@@ -102,21 +102,21 @@ void loop()
 
   // Imprimimos los datos del control de vuelo en la OLED
   display.clearDisplay();
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   display.println("CONTROL DE VUELO");
-  display.setCursor(0,20);
+  display.setCursor(0, 20);
   display.setTextSize(1);
   display.print("Angulo1: ");
-  display.setCursor(60,20);
+  display.setCursor(60, 20);
   display.print(ang1);
-  display.setCursor(0,30);
+  display.setCursor(0, 30);
   display.print("Angulo2: ");
-  display.setCursor(60,30);
+  display.setCursor(60, 30);
   display.print(ang2);
-  display.setCursor(0,50);
+  display.setCursor(0, 50);
   display.print("Enviando: ");
-  display.setCursor(60,50);
-  display.print(counter);      
+  display.setCursor(60, 50);
+  display.print(counter);
   display.display();
 
   // Esperamos 5 segundos
@@ -124,21 +124,21 @@ void loop()
   delay(5000);
 }
 
-void iniciar_Lora() 
+void iniciar_Lora()
 {
   //Pines SPI de LoRa
   SPI.begin(SCK, MISO, MOSI, SS);
-  
+
   //Pines LoRa
   LoRa.setPins(SS, RST, DIO0);
-  
+
   //Iniciamos LoRa
-  if (!LoRa.begin(BANDA)) 
+  if (!LoRa.begin(BANDA))
   {
     Serial.println("Error al inicial LoRa!");
     while (1);
   }
-  
+
   Serial.println("LoRa iniciada correctamente!");
   delay(2000);
-} 
+}
