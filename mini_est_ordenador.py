d18372303a3a1e638f17fbf56e0f6ca8e8ad1d0e
Argonautex <74357760+Argonautex@users.noreply.github.com>
2021-07-14 18:02:51 +0100

    Update mini_est_ordenador.py

--- mini_est_ordenador.py
+++ mini_est_ordenador.py
@@ -3,13 +3,20 @@ import time
 
 filename = 'datos_cansat.csv'
 with open(filename, 'a') as file_object:
-    file_object.write('clave,temperaturaBME,presionBME,humedadBME,altitudBME,DUV,CO2,GasesVolatiles,AcelerometroX,AcelerometroY,AcelerometroZ,GiroscopioX,GiroscopioY,GiroscopioZ,MagnetometroX,MagnetometroY,MagnetometroZ,presAlt,tempAlt,altAlt,bateria,IR,LatitudGPS,LongitudGPS,velocidadGPS,altitudGPS\n')
+    file_object.write('clave,temperaturaBME,presionBME,humedadBME,altitudBME,DUV,CO2,GasesVolatiles,AcelerometroX,'
+                      'AcelerometroY,AcelerometroZ,GiroscopioX,GiroscopioY,GiroscopioZ,MagnetometroX,MagnetometroY,'
+                      'MagnetometroZ,presAlt,tempAlt,altAlt,bateria,IR,LatitudGPS,LongitudGPS,velocidadGPS,'
+                      'altitudGPS\n')
     file_object.close()
 while True:
-    arduino = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=1.0)
+    arduino = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=1.0)
     line = arduino.readline()
-    lectura = str(line, 'UTF-8')
-    with open(filename, 'a') as file_object:
-        file_object.write(lectura)
-        print(lectura)
-        file_object.close()
+    try:
+        lectura = str(line, 'UTF-8')
+        with open(filename, 'a') as file_object:
+            file_object.write(lectura)
+            print(lectura)
+            file_object.close()
+    except:
+        print("ERROR: ", line)
+
